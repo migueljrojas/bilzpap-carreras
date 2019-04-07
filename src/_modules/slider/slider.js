8b4bea7fc53cc24f76a71cb345b9bf0c24f032eb
Your Name <you@example.com>
2019-04-07 18:26:24 -0300

    updates

--- src/_modules/slider/slider.js
+++ src/_modules/slider/slider.js
@@ -3,13 +3,47 @@
 // Constructor
 var Slider = function() {
     var slider = $('._slider');
+    var sliderMulti = $('._slidermulti');
+
     if (slider) {
         slider.each(function(){
             $(this).slick({
                 dots: true,
                 fade: true,
-                arrows:  false,
+                arrows: true,
                 autoplay: true
+            });
+        });
+    }
+
+    if (sliderMulti) {
+        sliderMulti.each(function(){
+            $(this).slick({
+                dots: false,
+                infinite: true,
+                speed: 300,
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                arrows: true,
+                autoplay: true,
+                responsive: [
+                    {
+                        breakpoint: 900,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2
+                        }
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: {
+                            centerMode: false,
+                            slidesToShow: 1,
+                            arrows: true,
+                            slidesToScroll: 1
+                        }
+                    }
+                ]
             });
         });
     }
